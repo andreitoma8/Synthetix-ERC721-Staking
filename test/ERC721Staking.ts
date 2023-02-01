94478334f045e00468dbd7b232dcec637efe41eb
andreitoma8 <andrei.toma8@yahoo.com>
2023-02-01 05:37:08 +0200

    added withdrawal tests

--- test/ERC721Staking.ts
+++ test/ERC721Staking.ts
@@ -143,9 +143,9 @@ describe("ERC721Staking", () => {
       await ethers.provider.send("evm_mine", []);
 
       await rewardToken.mint(stakingContract.address, rewardsAmount);
+
       const tx = await stakingContract.addRewardAmount(rewardsAmount);
       const receipt = await tx.wait();
-
       const txTimestamp = (await ethers.provider.getBlock(receipt.blockNumber))
         .timestamp;
 
@@ -158,6 +158,154 @@ describe("ERC721Staking", () => {
       expect(await stakingContract.rewardRate()).to.equal(
         leftoverRewards.add(rewardsAmount).div(stakingTimeLeft)
       );
+    });
+  });
+
+  describe("Staking", () => {
+    beforeEach(async () => {
+      await nftCollection.mint(alice.address, 3);
+      await nftCollection
+        .connect(alice)
+        .setApprovalForAll(stakingContract.address, true);
+    });
+
+    it("should not allow to stake if array param is empty", async () => {
+      await expect(stakingContract.connect(alice).stake([])).to.be.revertedWith(
+        "Staking: No tokenIds provided"
+      );
+    });
+
+    it("should stake correctly", async () => {
+      const tokensOfAlice = await nftCollection.tokensOfOwner(alice.address);
+      const totalStakedSupply = await stakingContract.totalStakedSupply();
+
+      await expect(stakingContract.connect(alice).stake(tokensOfAlice))
+        .to.emit(stakingContract, "Staked")
+        .withArgs(alice.address, tokensOfAlice);
+
+      expect(
+        await nftCollection.tokensOfOwner(stakingContract.address)
+      ).to.deep.equal(tokensOfAlice);
+      const aliceStakeInfo = await stakingContract.userStakeInfo(alice.address);
+      expect(aliceStakeInfo[0]).to.deep.equal(tokensOfAlice);
+      for (let i = 0; i < tokensOfAlice.length; i++) {
+        expect(
+          await stakingContract.stakedAssets(tokensOfAlice[i])
+        ).to.deep.equal(alice.address);
+      }
+      expect(await stakingContract.totalStakedSupply()).to.equal(
+        totalStakedSupply.add(tokensOfAlice.length)
+      );
+    });
+  });
+
+  describe("Withdrawing", () => {
+    beforeEach(async () => {
+      await nftCollection.mint(alice.address, 3);
+      await nftCollection
+        .connect(alice)
+        .setApprovalForAll(stakingContract.address, true);
+      await stakingContract
+        .connect(alice)
+        .stake(await nftCollection.tokensOfOwner(alice.address));
+    });
+
+    it("should not allow to withdraw if array param is empty", async () => {
+      await expect(
+        stakingContract.connect(alice).withdraw([])
+      ).to.be.revertedWith("Staking: No tokenIds provided");
+    });
+
+    it("should not allow to withdraw if array param contains non-staked tokens", async () => {
+      await expect(stakingContract.connect(alice).withdraw([7])).to.be.reverted;
+    });
+
+    it("should not allow withdrawing someone else's staked tokens", async () => {
+      const aliceStakeInfo = await stakingContract.userStakeInfo(alice.address);
+
+      await expect(
+        stakingContract.connect(bob).withdraw(aliceStakeInfo[0])
+      ).to.be.revertedWith("Staking: Not the staker of the token");
+    });
+
+    it("should withdraw correctly", async () => {
+      const aliceStakeInfo = await stakingContract.userStakeInfo(alice.address);
+      const aliceStakedTokens = aliceStakeInfo[0];
+      const totalStakedSupply = await stakingContract.totalStakedSupply();
+
+      await expect(stakingContract.connect(alice).withdraw(aliceStakedTokens))
+        .to.emit(stakingContract, "Withdrawn")
+        .withArgs(alice.address, aliceStakedTokens);
+
+      expect(await nftCollection.tokensOfOwner(alice.address)).to.deep.equal(
+        aliceStakedTokens
+      );
+      expect(await stakingContract.totalStakedSupply()).to.equal(
+        totalStakedSupply.sub(aliceStakedTokens.length)
+      );
+      for (let i = 0; i < aliceStakedTokens.length; i++) {
+        expect(
+          await stakingContract.stakedAssets(aliceStakedTokens[i])
+        ).to.deep.equal(ethers.constants.AddressZero);
+      }
+      expect(await stakingContract.userStakeInfo(alice.address)).to.deep.equal([
+        [],
+        0,
+      ]);
+    });
+
+    it("should withdraw correctly when just a couple of tokens are withdrawn", async () => {
+      const aliceStakeInfo = await stakingContract.userStakeInfo(alice.address);
+      const aliceStakedTokens = aliceStakeInfo[0];
+      const totalStakedSupply = await stakingContract.totalStakedSupply();
+
+      await stakingContract.connect(alice).withdraw([aliceStakedTokens[0]]);
+
+      expect(await nftCollection.tokensOfOwner(alice.address)).to.deep.equal([
+        aliceStakedTokens[0],
+      ]);
+      expect(await stakingContract.totalStakedSupply()).to.equal(
+        totalStakedSupply.sub(1)
+      );
+      expect(
+        await stakingContract.stakedAssets(aliceStakedTokens[0])
+      ).to.deep.equal(ethers.constants.AddressZero);
+
+      const newAliceStakeInfo = await stakingContract.userStakeInfo(
+        alice.address
+      );
+      const newAliceStakedTokens = newAliceStakeInfo[0];
+      expect(newAliceStakedTokens).to.have.lengthOf(
+        newAliceStakedTokens.length
+      );
+      expect(newAliceStakedTokens).to.not.include(aliceStakedTokens[0]);
+      for (let i = 0; i < newAliceStakedTokens.length; i++) {
+        expect(newAliceStakedTokens).to.deep.include(aliceStakedTokens[i + 1]);
+      }
+    });
+
+    it("should withdraw correcly form withdraw all", async () => {
+      const aliceStakeInfo = await stakingContract.userStakeInfo(alice.address);
+      const aliceStakedTokens = aliceStakeInfo[0];
+      const totalStakedSupply = await stakingContract.totalStakedSupply();
+
+      await stakingContract.connect(alice).withdrawAll();
+
+      expect(await nftCollection.tokensOfOwner(alice.address)).to.deep.equal(
+        aliceStakedTokens
+      );
+      expect(await stakingContract.totalStakedSupply()).to.equal(
+        totalStakedSupply.sub(aliceStakedTokens.length)
+      );
+      for (let i = 0; i < aliceStakedTokens.length; i++) {
+        expect(
+          await stakingContract.stakedAssets(aliceStakedTokens[i])
+        ).to.deep.equal(ethers.constants.AddressZero);
+      }
+      expect(await stakingContract.userStakeInfo(alice.address)).to.deep.equal([
+        [],
+        0,
+      ]);
     });
   });
 });
